a3a3a09e5ac623425af4be63194876ce059c3b02
melfedawy17-dotcom <m.elfedawy17@gmail.com>
2026-09-21 16:38:07 +0300

    Add files via upload

--- assets/js/cms.js
+++ assets/js/cms.js
@@ -10,7 +10,7 @@
   };
   var RAW = 'https://raw.githubusercontent.com/' + CMS.owner + '/' + CMS.repo + '/' + CMS.branch + '/';
   var root = document.body.getAttribute('data-root') || './';
-  var VER = '20260920y';
+  var VER = '20260920z';
   function vs(p){ return p + (p.indexOf('?') > -1 ? '' : '?v=' + VER); }
 
   function cb() { return '?t=' + Date.now(); }
